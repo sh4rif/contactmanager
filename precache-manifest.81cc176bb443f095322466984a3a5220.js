4cd350599d26774277a16d2e6fd513304c7a47bb
Muhammad Usman Sharif <sh4rif@hotmail.co.uk>
2019-01-06 20:45:16 +0500

    Updates

---
+++ precache-manifest.81cc176bb443f095322466984a3a5220.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9a0338cf3b8a6b392e9",
+    "url": "/contactmanager/static/css/main.766b025f.chunk.css"
+  },
+  {
+    "revision": "f9a0338cf3b8a6b392e9",
+    "url": "/contactmanager/static/js/main.f9a0338c.chunk.js"
+  },
+  {
+    "revision": "96e8dcd2d8ac325985e4",
+    "url": "/contactmanager/static/css/1.d64ed8d3.chunk.css"
+  },
+  {
+    "revision": "96e8dcd2d8ac325985e4",
+    "url": "/contactmanager/static/js/1.96e8dcd2.chunk.js"
+  },
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "fceec6c9b464f083e2e4601ed1997637",
+    "url": "/contactmanager/index.html"
+  }
+];
