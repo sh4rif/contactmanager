af92fe7e68aa67cf6c11d983be93a2d5c5730f99
Muhammad Usman Sharif <sh4rif@hotmail.co.uk>
2019-02-02 23:05:48 +0500

    Added Search facility by typing in the search box

--- src/components/contacts/Contacts.js
+++ src/components/contacts/Contacts.js
@@ -3,39 +3,57 @@ import { Consumer } from "../../context";
 
 import Contact from "./Contact";
 
-const Contacts = props => {
-  // console.log(props);
-  return (
-    <Consumer>
-      {value => (
-        <Fragment>
-          <h1 className="display-4 mb-2">
-            <span className="text-danger">Contact</span> List
-          </h1>
-          <div className="card mb-3">
-            <div className="card-header">
-              <div className="row d-flex justify-content-between">
-                <div className="col-md-8">
-                  <h3>Contacts</h3>
+class Contacts extends React.Component {
+  state = { search: "" };
+
+  onSearch = e => {
+    this.setState({ search: e.target.value });
+  };
+
+  render() {
+    return (
+      <Consumer>
+        {value => {
+          let updatedContacts = value.contacts.filter(contact => {
+            return (
+              contact.name
+                .toLowerCase()
+                .indexOf(this.state.search.toLowerCase()) !== -1
+            );
+          });
+          // let updatedContacts = value.contacts;
+          return (
+            <Fragment>
+              <h1 className="display-4 mb-2">
+                <span className="text-danger">Contact</span> List
+              </h1>
+              <div className="card mb-3">
+                <div className="card-header">
+                  <div className="row d-flex justify-content-between">
+                    <div className="col-md-8">
+                      <h3>Contacts</h3>
+                    </div>
+                    <div className="col-md-4">
+                      <input
+                        className="form-control form-control-sm"
+                        placeholder="Search..."
+                        value={this.state.search}
+                        onChange={this.onSearch}
+                      />
+                    </div>
+                  </div>
                 </div>
-                <div className="col-md-4">
-                  <input
-                    className="form-control form-control-sm"
-                    placeholder="Search..."
-                  />
+                <div className="card-body">
+                  {updatedContacts.map(contact => (
+                    <Contact key={contact.id} contact={contact} />
+                  ))}
                 </div>
               </div>
-            </div>
-            <div className="card-body">
-              {value.contacts.map(contact => (
-                <Contact key={contact.id} contact={contact} />
-              ))}
-            </div>
-          </div>
-        </Fragment>
-      )}
-    </Consumer>
-  );
-};
-
+            </Fragment>
+          );
+        }}
+      </Consumer>
+    );
+  }
+}
 export default Contacts;
